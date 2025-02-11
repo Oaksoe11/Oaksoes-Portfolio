1673f9c4957db8097743ec72f5fd2f86006543f2
Oaksoe11 <osk@sfu.ca>
2025-02-10 23:54:30 -0800

    Add assets folder which contains icons, pngs and assets.js

---
+++ portfolio/assets/assets.js
@@ -0,0 +1,109 @@
+import user_image from './user-image.png';
+import code_icon from './code-icon.png';
+import edu_icon from './edu-icon.png';
+import project_icon from './project-icon.png';
+import vscode from './vscode.png';
+import firebase from './firebase.png';
+import figma from './figma.png';
+import git from './git.png';
+import mongodb from './mongodb.png';
+import right_arrow_white from './right-arrow-white.png';
+import logo from './logo.png';
+import mail_icon from './mail_icon.png';
+import profile_img from './profile_img.png';
+import profile_imgg from './profile_imgg.png';
+import download_icon from './download-icon.png';
+import hand_icon from './hand-icon.png';
+import header_bg_color from './header-bg-color.png';
+import moon_icon from './moon_icon.png';
+import sun_icon from './sun_icon.png';
+import arrow_icon from './arrow-icon.png';
+import menu_black from './menu-black.png';
+import menu_white from './menu-white.png';
+import close_black from './close-black.png';
+import close_white from './close-white.png';
+import web_icon from './web-icon.png';
+import mobile_icon from './mobile-icon.png';
+import ui_icon from './ui-icon.png';
+import graphics_icon from './graphics-icon.png';
+import right_arrow from './right-arrow.png';
+import send_icon from './send-icon.png';
+import right_arrow_bold from './right-arrow-bold.png';
+import instagram from './instagram.png';
+import linkedin from './social.png';
+import github from './github.png';
+
+export const assets = {
+    instagram,
+    linkedin,
+    github,
+    user_image,
+    code_icon,
+    edu_icon,
+    project_icon,
+    vscode,
+    firebase,
+    figma,
+    git,
+    mongodb,
+    right_arrow_white,
+    logo,
+    mail_icon,
+    profile_img,
+    profile_imgg,
+    download_icon,
+    hand_icon,
+    header_bg_color,
+    moon_icon,
+    sun_icon,
+    arrow_icon,
+    menu_black,
+    menu_white,
+    close_black,
+    close_white,
+    web_icon,
+    mobile_icon,
+    ui_icon,
+    graphics_icon,
+    right_arrow,
+    send_icon,
+    right_arrow_bold,
+};
+
+export const workData = [
+    {
+        title: 'Project #',
+        description: 'Web Design',
+        bgImage: '/work-1.png',
+    },
+    {
+        title: 'Project #',
+        description: 'Mobile App',
+        bgImage: '/work-2.png',
+    },
+    {
+        title: 'Project #',
+        description: 'Web Design',
+        bgImage: '/work-3.png',
+    },
+    {
+        title: 'Project #',
+        description: 'UI/UX Design',
+        bgImage: '/work-4.png',
+    },
+]
+
+export const infoList = [
+    { icon: assets.code_icon, title: 'Languages', description: 'C++, C, Java, HTML, CSS, Tailwind CSS, JavaScript, React Js, Next Js' },
+    { icon: assets.edu_icon, title: 'Education', description: 'B.Sc. in Computer Science (Software Systems), Expected 2028' },
+    { icon: assets.project_icon, title: 'Projects', description: 'Hands on experience with personal projects' }
+];
+
+export const toolsData = [
+    { image: assets.vscode, url: "https://code.visualstudio.com/" },
+    { image: assets.firebase, url: "https://firebase.google.com/" },
+    { image: assets.mongodb, url: "https://www.mongodb.com/" },
+    { image: assets.figma, url: "https://www.figma.com/" },
+    { image: assets.git, url: "https://git-scm.com/" },
+  ];
+
