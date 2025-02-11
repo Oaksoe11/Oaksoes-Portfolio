c3b45de7cb00b9c444582f1d5648aa6f2d786cf7
Oaksoe11 <osk@sfu.ca>
2025-02-11 00:14:43 -0800

    Add Navbar component with basic layout

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 'use client'
 
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
     <>
+      <Navbar/>
     </>
   );
 }
