36ad2e5498a08bce437f25aa45bc789eee5e14c0
Oaksoe11 <osk@sfu.ca>
2025-02-11 02:18:35 -0800

    Add h4 heading and implement styles

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
+    <div id="about" className='w-full px-[12%] py-2 scroll-mt-20 min-h-screen mt-5'>
+        <h4 className='text-center mb-2 text-lg font-Outfit'>
+            Introduction
+        </h4>
       
     </div>
   )
