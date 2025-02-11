9f84033a78001fc17e74ae76b4ba8e56df234084
Oaksoe11 <osk@sfu.ca>
2025-02-11 02:43:10 -0800

    Add h4 and h2 headings

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Projects = () => {
   return (
-    <div>
-      
+    <div id="projects" className='w-full px-[12%] py-6 mt-32 scroll-mt-20 min-h-screen'>
+        <h4 className='text-center mb-2 text-lg font-Outfit'>
+            My Portfolio
+        </h4>
+
+        <h2 className='text-center text-5xl font-Outfit'>
+            My latest work
+        </h2>
     </div>
   )
 }
