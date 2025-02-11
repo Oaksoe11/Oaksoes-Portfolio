07e3a49b061957da6346316fb8c655ffbe60f1ac
Oaksoe11 <osk@sfu.ca>
2025-02-11 01:42:28 -0800

    Fix mobile nav bar not opening

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -6,11 +6,11 @@ const Navbar = () => {
         const sideMenuRef = useRef();
     
         const openMenu = () => {
-            sideMenuRef.current.style.transform = 'translateX(-16rem)'
+            sideMenuRef.current.style.transform = 'translateX(-16rem)';
         }
     
         const closeMenu = () => {
-            sideMenuRef.current.style.transform = 'translateX(16rem)'
+            sideMenuRef.current.style.transform = 'translateX(16rem)';
         }
     
         const [isScroll, setIsScroll] = useState(false);
@@ -25,15 +25,16 @@ const Navbar = () => {
         },[])
 
   return (
-    <div>
-        <div className = 'fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
-            <Image src={assets.header_bg_color} alt="" className = 'w-full' />
-        </div>
-
-        <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4
-        flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm": ""}`}>
+    <>
+    <div className = 'fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+        <Image src={assets.header_bg_color} alt="" className = 'w-full' />
+    </div>
+      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4
+      flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm": ""}`}>
         <a href="#top">
-        <h1 className="text-4xl font-bold text-g'ray-900 cursor-pointer">
+            {/* <Image src={assets.logo} 
+            alt='' className = 'w-20 cursor-pointer mr-14' /> */}
+            <h1 className="text-4xl font-bold text-g'ray-900 cursor-pointer">
             OakSoe<span className="text-blue-400">.</span>
             </h1>
         </a>
@@ -48,7 +49,12 @@ const Navbar = () => {
         </ul>
 
         <div className='flex items-center gap-4'>
-        <a href="#contact" className='hidden lg:flex 
+
+            {/* <button>
+                <Image src={assets.moon_icon} alt='' className='w-6' />
+            </button> */}
+
+            <a href="#contact" className='hidden lg:flex 
             items-center gap-3 px-10 py-2.5 border border-gray-500
             rounded-full ml-4'>
                 Contact 
@@ -60,14 +66,16 @@ const Navbar = () => {
                 <Image src={assets.menu_black} alt='' className='w-6' />
             </button>
         </div>
+
         {/* ########## mobile menu ########## */}
 
-        <ul className='flex md:hidden flex-col gap-4 py-20 px-10 fixed
+        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed
         -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-blue-50 transition
         duration-500'> 
 
-            <div className='absolute right-6 top-6 ' onClick={closeMenu}>
-                <Image src={assets.close_black} alt='' className='w-5 cursor-pointer'/>
+            <div className='absolute right-6 top-6' onClick={closeMenu}>
+                <Image src={assets.close_black} alt='' className='w-5
+                cursor-pointer'/>
             </div>
             <li><a onClick={closeMenu} href="#top">Home</a></li>
             <li><a onClick={closeMenu} href="#about">About</a></li>
@@ -75,8 +83,8 @@ const Navbar = () => {
             <li><a onClick={closeMenu} href="#projects">My Projects</a></li>
             <li><a onClick={closeMenu} href="#contact">Contact me</a></li>
         </ul>
-        </nav>
-    </div>
+      </nav>
+    </>
   )
 }
 
