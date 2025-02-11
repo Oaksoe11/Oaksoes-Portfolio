ee129111c351634fa6445101febbfe8f6f5bf1b2
Oaksoe11 <osk@sfu.ca>
2025-02-11 02:23:01 -0800

    Add heading and profile image
    - Adjust spacing using flex
    - Import profile image
    - Implement styles on Image and Headings to look more appealing

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,3 +1,5 @@
+import { assets } from '@/assets/assets'
+import Image from 'next/image'
 import React from 'react'
 
 const About = () => {
@@ -6,7 +8,19 @@ const About = () => {
         <h4 className='text-center mb-2 text-lg font-Outfit'>
             Introduction
         </h4>
-      
+
+        <h2 className='text-center text-5xl font-Outfit'>
+            About me
+        </h2>
+
+        <div className='flex w-full flex-col lg:flex-row items-center
+        gap-20 mt-8'>
+            <div className='w-64 sm:w-80 rounded-3xl max-w-none'>
+                <Image src={assets.profile_img} alt='user'
+                className='w-full rounded-3xl filter contrast-115'/>
+            </div>
+
+        </div>
     </div>
   )
 }
