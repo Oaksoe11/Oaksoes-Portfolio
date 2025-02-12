24b55ac991b5f5a8d16771bbe4765d3fd0866c6c
Oaksoe11 <osk@sfu.ca>
2025-02-11 17:47:24 -0800

    Fix typo contact -> Contact

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
-import Contact from "./components/contact";
+import Contact from "./components/Contact";
 
 export default function Home() {
   return (
