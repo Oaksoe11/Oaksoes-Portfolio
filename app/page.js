85638cf501ee5f3fb100964d742b3d777d2ccf8a
Oaksoe11 <osk@sfu.ca>
2025-02-11 17:42:00 -0800

    Add contact tag

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Navbar from "./components/Navbar";
 import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
+import Contact from "./components/contact";
 
 export default function Home() {
   return (
@@ -13,6 +14,7 @@ export default function Home() {
       <Header/>
       <About/>
       <Projects/>
+      <Contact/>
     </>
   );
 }
