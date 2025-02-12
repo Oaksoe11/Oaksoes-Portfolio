04b0c0ed634669e2ac930573b32bf986e537cfe4
Oaksoe11 <osk@sfu.ca>
2025-02-11 17:44:19 -0800

    Add contact form submission
    - Implemented a contact form component using React.
    - Added form submission logic with Web3Forms API.
    - Included state management for form submission results.
    - Handled form reset and error messages on successful/unsuccessful submissions.

--- app/components/Contact.jsx
+++ app/components/Contact.jsx
@@ -1,6 +1,32 @@
 import React from 'react'
 
-const contact = () => {
+const Contact = () => {
+
+    const [result, setResult] = useState("");
+
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "d0a50088-021c-4426-9935-cdfe43445bfb");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    }; 
+    
   return (
     <div>
       
@@ -8,4 +34,4 @@ const contact = () => {
   )
 }
 
-export default contact
+export default Contact
