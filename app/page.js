5fdd10901bea1e83667d8598cde56e9e2c0f355c
Oaksoe11 <osk@sfu.ca>
2025-02-11 20:14:03 -0800

    Add footer tag

--- app/page.js
+++ app/page.js
@@ -6,6 +6,7 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
@@ -15,6 +16,7 @@ export default function Home() {
       <About/>
       <Projects/>
       <Contact/>
+      <Footer/>
     </>
   );
 }
